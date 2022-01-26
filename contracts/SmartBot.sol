03a867b6cf857dc86156497248d962ba9ca09073
AnneC <chretien.an@gmail.com>
2022-01-26 15:57:31 +0100

    Portfolio Project - Bitcoin Trading Bot

---
+++ contracts/SmartBot.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6;
+
+// first, get Binance test token using Metamask
+
+contract SmartBot {
+    enum STATE {
+        BUY,
+        SELL
+    }
+}
