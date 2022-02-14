de5298ecf8c5f9d6ab096bca4f71d9c0d5b2086e
AnneC <chretien.an@gmail.com>
2022-02-14 10:56:28 +0100

    compiling issues before running get_weth script

--- BigEnsemble/scripts/deployBot.py
+++ BigEnsemble/scripts/deployBot.py
@@ -2,12 +2,6 @@
 # try to guarantee that the prices are correct before triggering the smart contract\
 
 
-UPWARD_TREND_THRESHOLD = 1.5
-DIP_THRESHOLD = -2
-PROFIT_THRESHOLD = 1.25
-STOP_LOSS_THRESHOLD = -2
-
-
 def start_bot():
     pass
 
