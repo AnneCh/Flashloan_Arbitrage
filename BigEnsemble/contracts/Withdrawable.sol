1a31564f60c6ca4825a6ae9a93af642d4984f4b1
Anne Chretien <chretien.an@gmail.com>
2022-02-18 11:23:56 +0100

    trying to fix comppiling issue

--- BigEnsemble/contracts/Withdrawable.sol
+++ BigEnsemble/contracts/Withdrawable.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "aave/contracts/dependencies/openzeppelin/contracts/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
