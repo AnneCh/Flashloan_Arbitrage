62a60a435113d4f41aaa38bacec449bf0c2ebf40
Anne Chretien <chretien.an@gmail.com>
2022-06-21 14:37:46 +0200

    running first script + log

--- OUTDated/Essai N1/scripts/get_weth.py
+++ OUTDated/Essai N1/scripts/get_weth.py
@@ -1,9 +1,3 @@
-# mints WETH by depositing ETH
-# I need WETH to pay for Aave's premium fees for using their pool through flashloans
-# This script will have me use my metamask wallet through the rinkeby test network
-# to get 1 WETH
-# I'm still wondering how to write that to not involve my own wallet, but whoever's
-# wallet will be connected as a user of the Bot
 from brownie import accounts, config, network, interface
 
 
@@ -11,9 +5,10 @@ def main():
     get_weth()
 
 
+# can replace the accounts.add
 def get_weth():
-    acc = accounts.add(config["wallets"]["from_key"])
+    myAccount = accounts.add(config["wallets"]["from_key"])
     weth = interface.WethInterface(config["networks"][network.show_active()]["weth"])
-    tx = weth.deposit({"from": acc, "value": 200000000000000000})
-    print("Received 0.2 WETH")
+    tx = weth.deposit({"from": myAccount, "value": 1000000000000})
+    print("Received 0.000001 WETH")
     return tx
