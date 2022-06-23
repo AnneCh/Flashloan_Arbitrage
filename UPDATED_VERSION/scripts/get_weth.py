5183c4ce938d6f6372fdc5e661ff049d2fbd1db4
Anne Chretien <chretien.an@gmail.com>
2022-06-23 14:21:17 +0200

    connect to weth interface

--- UPDATED_VERSION/scripts/get_weth.py
+++ UPDATED_VERSION/scripts/get_weth.py
@@ -1,6 +1,13 @@
+from scripts.helpful import get_account, get_wethInterface
+
+
 def main():
     get_weth()
-    
+
 
 def get_weth():
-    
+    accountA = get_account()
+    weth = get_wethInterface()
+    tx = weth.deposit({"from": accountA, "value": 10000000000000000})
+    print("Yeah you made it!")
+    return tx
--- UPDATED_VERSION/scripts/helpful.py
+++ UPDATED_VERSION/scripts/helpful.py
@@ -1,5 +1,9 @@
-from brownie import network, accounts, config
+from brownie import network, accounts, config, interface
 
 
 def get_account():
     return accounts.add(config["wallets"]["from_key"])
+
+
+def get_wethInterface():
+    return interface.WethInterface(config["networks"][network.show_active()]["weth"])
