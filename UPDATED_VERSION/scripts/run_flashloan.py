35a7302332a007bc28ce88fa26bbfd04c069807e
Anne Chretien <chretien.an@gmail.com>
2022-06-27 15:20:13 +0200

    added gas_limit

--- UPDATED_VERSION/scripts/run_flashloan.py
+++ UPDATED_VERSION/scripts/run_flashloan.py
@@ -4,7 +4,7 @@ from scripts.helpful import get_account, get_wethInterface
 # set up global variables that will reflect the minimum WETH balance required in order for the
 # caller to have enough WETH to pay the fees
 # and one to ge the URL base to display the transaction when sucessful
-MINIMUM_FLASHLOAN_WETH_BALANCE = 10000000000000000
+MINIMUM_FLASHLOAN_WETH_BALANCE = 9000000000000000
 ETHERSCAN_TX_URL = "https://kovan.etherscan.io/tx/{}"
 
 
@@ -20,7 +20,9 @@ def main():
     ):  # check that our wallet (accountCaller) has enough WETH for the gas fees
         print("Giving some of your WETH away to the contract, for gas.")
         weth.transfer(
-            flashloan, "20 ether", {"from": accountCaller}
+            flashloan,
+            "20 ether",
+            {"from": accountCaller, "gas_limit": 1200000, "allow_revert": True},
         )  # borrow 20ETH in flashloan
     print("Executing flashloan...Be Patient!!")
     tx = flashloan.flashloan(
--- UPDATED_VERSION/tests/unit_tests.py
+++ UPDATED_VERSION/tests/unit_tests.py
@@ -9,4 +9,14 @@ def test_account():
     assert test_account == "0x4a8e4183709bb59E0807D64E46cFe7747479d371"
 
 
+def weth_balance():
+    # check that we have funded the contract with enough WETH
+    pass
+
+
+def borrow_ten_eth():
+    # try to borrow 10ETH
+    pass
+
+
 # first test passed!
