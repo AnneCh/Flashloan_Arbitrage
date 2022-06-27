601723275f225e8ba29e8933e27ec96a2fab5618
Anne Chretien <chretien.an@gmail.com>
2022-06-27 15:53:29 +0200

    fixture and tests lists

--- UPDATED_VERSION/tests/unit_tests.py
+++ UPDATED_VERSION/tests/unit_tests.py
@@ -1,12 +1,41 @@
 from scripts.helpful import get_account
+import pytest
+from brownie import Flashloan_logic
 
 # unit tests
 # test that the address of account deployed is my test_account address
 
+# use fixture to automatically run function prior to other tests - good for initialisation
+@pytest.fixture(autouse=True)
+def setup(fn_isolation):
+    """
+    Isolation setup fixture.
+    This ensures that each test runs against the same base environment.
+    """
+    acct = get_account()
+
+
+@pytest.fixture(scope="module")
+def aave_lending_pool_v2(Contract):
+    """
+    Yield a `Contract` object for the Aave lending pool address provider. KOVAN ADDRESS
+    """
+    yield Contract("0x88757f2f99175387ab4c6a4b3067c77a695b0349")
+
+
+@pytest.fixture(scope="module")
+def flashloan(Flashloan_logic, aave_lending_pool_v2, acct):
+    """
+    Deploy a `Flashloan_logic` contract and yields the generated object.
+    """
+    yield Flashloan_logic.deploy(aave_lending_pool_v2, {"from": acct})
+
 
 def test_account():
-    test_account = get_account()
     assert test_account == "0x4a8e4183709bb59E0807D64E46cFe7747479d371"
+
+
+# first test passed!
 
 
 def weth_balance():
@@ -19,4 +48,10 @@ def borrow_ten_eth():
     pass
 
 
-# first test passed!
+def add_index():
+    # make sure new address is added to the index
+    pass
+
+
+def withdraw_funds():
+    pass
