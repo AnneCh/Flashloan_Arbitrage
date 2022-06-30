6d3b9426643ae94cdbb2a32dc3baab3b697b94f2
Anne Chretien <chretien.an@gmail.com>
2022-06-30 12:21:01 +0200

    modif numbers

--- UPDATED_VERSION/scripts/run_flashloan.py
+++ UPDATED_VERSION/scripts/run_flashloan.py
@@ -4,7 +4,7 @@ from scripts.helpful import get_account, get_wethInterface
 # set up global variables that will reflect the minimum WETH balance required in order for the
 # caller to have enough WETH to pay the fees
 # and one to ge the URL base to display the transaction when sucessful
-MINIMUM_FLASHLOAN_WETH_BALANCE = 300000000000000000
+MINIMUM_FLASHLOAN_WETH_BALANCE = 600000000000000000
 ETHERSCAN_TX_URL = "https://kovan.etherscan.io/tx/{}"
 
 
@@ -28,7 +28,7 @@ def main():
     tx = flashloan.userFlashloan(
         weth,
         "2 ether",
-        {"from": accountCaller, "gas_limit": 1200000, "allow_revert": True},
+        {"from": accountCaller, "gas_limit": 20000000, "allow_revert": True},
     )
     print(
         "Youhouuuuuuuuuu!!! Here is the link to your transaction: "
